cf45be6369dfbd8d9f896dcb53de373bb0b769d0
ahnjehoon <ahnjehoon@naver.com>
2024-04-02 21:18:48 +0900

    Hello World

---
+++ src/main/java/com/jehoon/tutorial/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.jehoon.tutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String get() {
+        return "Hello World";
+    }
+}

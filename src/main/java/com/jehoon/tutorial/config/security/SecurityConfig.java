a18a1585a345936bc8c82e043a4aede7eb7ac70b
ahnjehoon <ahnjehoon@naver.com>
2024-04-16 21:15:52 +0900

    security-2

--- src/main/java/com/jehoon/tutorial/config/security/SecurityConfig.java
+++ src/main/java/com/jehoon/tutorial/config/security/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.jehoon.tutorial.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -23,5 +28,27 @@ public class SecurityConfig {
                 .passwordEncoder(str -> passwordEncoder().encode(str))
                 .build();
         return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+
+                // CORS CSRF 미사용처리
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // 인증 활성화
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+
+                // Form Login 활성화
+                .formLogin(Customizer.withDefaults())
+
+                // 동일도메인 iframe 접근 활성화
+                .headers(header -> header
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+
+                .build();
     }
 }
